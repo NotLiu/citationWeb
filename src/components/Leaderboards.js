72ffaf53666d8ae1d01963458cfcd34498230ccc
ZaneFadul <theboonandthezane@gmail.com>
2021-09-27 22:07:31 -0400

    working on db stuff hehe

--- src/components/Leaderboards.js
+++ src/components/Leaderboards.js
@@ -4,6 +4,7 @@ import Select from "react-select";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, query, where} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,8 +22,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app)
 console.log(analytics);
 
+const getAll = async db => {
+  const dataRef = collection(db, 'citation-34f48-default-rtdb')
+
+}
 export default function Leaderboards({
   boardType,
   setBoardType,
